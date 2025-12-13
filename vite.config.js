81fdf02f5306ad9b1ab6bd0215d5ce4ca983011c
KEAN CHANTHY <kean.chanthy.1222@rupp.edu.kh>
2025-12-13 18:13:14 +0700

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Click2Eat-Admin/",
+  base: process.env.CLICK_2_EAT || "/Click2Eat-Admin",
 });
